5e1d2c49eac7512ca46ba91a8216bc0575825250
Mark Mueller <92736704+MuellMark@users.noreply.github.com>
2023-02-17 20:37:36 -0600

    Update main.js

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,6 +1,6 @@
 clickme.addEventListener('click',function(){
     alert('AUUUUUUGHH');
 })
-// clickme2.addEventListener('click',function(){
-//     alert('Whadahell');
-// })
+clickme2.addEventListener('click',function(){
+    alert('Whadahell');
+})
